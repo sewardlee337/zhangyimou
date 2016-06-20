d5ecb7980e7fa557f3ca1677262c147594d6d77c
Seward Lee <sewardlee337@gmail.com>
2016-06-19 22:06:21 -0700

    Add R package materials

---
+++ R/colors.R
@@ -0,0 +1,28 @@
+## Each vector contains hexadecimal codes for color palette corresponding to respective movie.
+
+# House of Flying Daggers
+flying_daggers <- c("#1C1C2F", "#DFDCD7", "#8FA1A8", "#3E5866", "#95464D", "#C67778")
+
+# Raise the Red Lantern
+red_lantern <- c("#43150A", "#6E2916", "#1A0905", "#C23915", "#884E36", "#D97B3A")
+
+# The Road Home
+road_home <- c("#EFDAA9", "#F3F1E8", "#D492B1", "#9C7D78", "#24202F", "#AF3220")
+
+# Not One Less
+one_less <- c("#7286A0", "#A29FB9", "#516E7A", "#172427", "#C8C2D6", "#9C5E52")
+
+# Riding Alone for a Thousand Miles
+riding_alone <- c("#1F2D50", "#A7D6EF", "#EDE3DE", "#6C5455", "#A49395", "#55B7E4")
+
+# Curse of the Golden Flower
+golden_flower <- c("#5E2C0C", "#744626", "#936A25", "#2C1B0E", "#A17F60", "#C6BA9A")
+
+# Under the Hawthorne Tree
+hawthorne_tree <- c("#DFD581", "#7D8A84", "#B0B063", "#5E7842", "#1D372E", "#1F4C7E")
+
+# Hero (Red)
+hero_red <- c("#68130D", "#992B19", "#290A07", "#AC6E53")
+
+# Hero (Blue)
+hero_blue <- c("#364A71", "#6072A8", "#142234", "#9EB2EE")
