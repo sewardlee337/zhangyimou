94eebd2c39068af929955d4ae6d3b74bfa0efb7c
sewardlee337 <sewardlee337@gmail.com>
2016-06-20 21:35:13 -0700

    Remove color names

--- R/palette_sample.R
+++ R/palette_sample.R
@@ -4,32 +4,30 @@
 color_palettes <- list(
 
      # House of Flying Daggers
-     FlyingDaggers = FlyingDaggers <- data.frame(
-          hexcodes = c("#C67778", "#3E5866", "#95464D", "#8FA1A8", "#DFDCD7", "#1C1C2F"),
-          approx.name = c("Contessa", "Fiord", "Copper Rust", "Regent Gray", "Westar", "Mirage"),
-          stringsAsFactors = FALSE),
+     FlyingDaggers = c("#C67778", "#3E5866", "#95464D", "#8FA1A8", "#DFDCD7", "#1C1C2F"),
 
      # The Road Home
-     RoadHome = RoadHome <- data.frame(
-          hexcodes = c("#EFDAA9", "#D492B1", "#F3F1E8", "#AF3220", "#9C7D78", "#24202F"),
-          approx.name = c("Double Colonial White", "Can Can", "Spring Wood", "Roof Terracotta", "Pharlap", "Steel Gray"),
-          stringsAsFactors = FALSE)
+     RoadHome = c("#D492B1", "#F3F1E8", "#AF3220", "#9C7D78", "#24202F", "#EFDAA9"),
+
+     # Not One Less
+     OneLess = c("#9CA6B7", "#8E644F", "#383214", "#565030", "#C28A79", "#CCC8C9"),
+
+     # Raise the Red Lantern
+     RedLantern = c("#C23915", "#D97B3A", "#6E2916", "#884E36", "#1A0905", "#43150A")
 
 )
 
 #    Create a data frame matching movie names to abbreviations
 
-name_table <- data.frame(abbr = c("FlyingDaggers", "RoadHome"), fullname = c("House of Flying Daggers", "The Road Home"))
+name_table <- data.frame(abbr = c("FlyingDaggers", "RoadHome", "OneLess", "RedLantern"),
+                         fullname = c("House of Flying Daggers", "The Road Home", "Not One Less", "Raise the Red Lantern"))
 
 
 display_palette <- function(name) {
 
      #    Extract colors and names
 
-     chosen_palette <- color_palettes[[name]]
-
-     hexcodes <- chosen_palette$hexcodes
-     names <- chosen_palette$approx.name
+     hexcodes <- color_palettes[[name]]
 
      title <- paste("Color Palette:\n", name_table$fullname[which(name_table$abbr == name)])
 
@@ -37,9 +35,7 @@ display_palette <- function(name) {
 
      ddf <- data.frame(var1 = c("aa", "bb", "cc", "dd", "ee", "ff"), var2 = c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6))
 
-     ddf <- cbind(ddf, names)
-
-     ggplot(ddf, aes(1, var2, fill=names, order = var1)) + geom_bar(stat="identity") + scale_fill_manual(values = hexcodes) +
+     ggplot(ddf, aes(1, var2, fill=var1)) + geom_bar(stat="identity") + scale_fill_manual(values = hexcodes) +
           coord_flip() +
           theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
                 axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
@@ -49,3 +45,9 @@ display_palette <- function(name) {
 
 }
 
+
+choose_palette <- function(name){
+
+     return(color_palettes[[name]])
+
+}
